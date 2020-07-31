1060a18481f93a908e0a21e91a214adfa9316163
Carlo <carlo@carlos-air.home>
2020-07-31 09:51:24 +0100

    lle update

--- commands/publicAccess/8ball.js
+++ commands/publicAccess/8ball.js
@@ -19,10 +19,10 @@ module.exports = {
 
         const Embed1 = new Discord.MessageEmbed()
             .setTitle(`${emoji(`${InformationEmoji}`)}` + " **COMMAND INFO**")
-            .addField("COMMAND", "```8ball```", true)
-            .addField("PERMISSIONS", "```None```", true)
-            .addField("USAGE", "```>8ball <message>```", true)
-            .addField("DESCRIPTION", "```Randomizes a response from 20 different messages from the real 8ball.```", true)
+            .addField("COMMAND", "`8ball`", true)
+            .addField("PERMISSIONS", "`None`", true)
+            .addField("USAGE", ">8ball <message>`", true)
+            .addField("DESCRIPTION", "`Randomizes a response from 20 different messages from the real 8ball.", true)
             .setColor(InformationColor)
 
         let questions = message.content.slice(bot.prefix.length + 6)
