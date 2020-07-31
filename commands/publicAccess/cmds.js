ee2e19b0997d46dd4754117ce770860b7ec395ae
Carlo <carlo@carlos-air.home>
2020-07-31 13:44:21 +0100

    New update

--- commands/publicAccess/cmds.js
+++ commands/publicAccess/cmds.js
@@ -60,9 +60,9 @@ module.exports = {
             }
 
             const msg2 = await message.channel.send(Embed2).then(async msg2 => {
-                const Emoji = await promptMessage(msg2, message.author, time, ["⬅️"]);
+                const Emoji2 = await promptMessage(msg2, message.author, time, ["⬅️"]);
 
-                if (Emoji === "⬅️") {
+                if (Emoji2 === "⬅️") {
 
                     msg2.reactions.removeAll().then(async msg2 => {
                         msg2.edit(Embed1)
