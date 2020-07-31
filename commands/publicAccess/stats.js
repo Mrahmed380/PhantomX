d6a32e8af5931b93ae25d2c0384800b09637ab07
Carlo <carlo@carlos-air.home>
2020-07-31 11:23:41 +0100

    New update

--- commands/publicAccess/stats.js
+++ commands/publicAccess/stats.js
@@ -30,7 +30,7 @@ module.exports = {
             sec = Math.floor((minutesms) / (1000));
             return days + "d " + hours + "h " + minutes + "m " + sec + "s";
         }
-
+//
         const Embed = new Discord.MessageEmbed()
             .setTitle("**SYSTEM STATUS**")
             .setColor(0xFAFAFA)
