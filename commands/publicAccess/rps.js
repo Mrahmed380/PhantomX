93ad5b4f6d033062f91a17a0075f1ca4c33c58b6
Carlo <carlo@carlos-air.home>
2020-07-31 12:16:27 +0100

    New update

--- commands/publicAccess/rps.js
+++ commands/publicAccess/rps.js
@@ -35,6 +35,7 @@ module.exports = {
         await m.reactions.removeAll();
 
         const Embed = new Discord.MessageEmbed()
+        .setTitle("GAME STATS")
             .addField(Result, `${Reacted} vs ${BotOptions}`)
             .setFooter("You       Bot");
 
