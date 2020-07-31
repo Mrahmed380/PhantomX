4da67c05f1b04ff344779b8ac09e807b11211cad
Carlo <carlo@carlos-air.home>
2020-07-31 12:04:20 +0100

    New update

--- handlers/command.js
+++ handlers/command.js
@@ -30,16 +30,3 @@ module.exports = (bot) => {
     console.log(table.toString());
 }
 
-
-const { readdirSync } = require("fs");
-
-module.exports = (client) => {
-    readdirSync("./commands/").filter(x => x.includes(".DS_Store") == false).map(dir => {
-       const commands = readdirSync(`./commands/${dir}/`).filter(f => f.includes(".DS_store") == false).map(cmd=>{
-           let pull = require(`../commands/${dir}/${cmd}`)
-           console.log(`Loaded command ${pull.name}`)
-           client.commands.set(pull.name,pull)
-           if (pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => client.aliases.set(alias, pull.name));
-       })
-    })
-}
