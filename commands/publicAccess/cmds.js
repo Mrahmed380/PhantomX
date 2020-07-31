28b9c1a71eb50b03d3a16a0c0a33520509189f49
Carlo <carlo@carlos-air.home>
2020-07-31 13:08:46 +0100

    New update

--- commands/publicAccess/cmds.js
+++ commands/publicAccess/cmds.js
@@ -14,7 +14,7 @@ module.exports = {
             const LEmoji = await promptMessage(msg, message.author, time, ["➡️"]);
 
             if (LEmoji === "➡️") {
-                let msg = await message.edit(privateCommandsImage);
+                await msg.edit(privateCommandsImage);
 
                 message.reactions.removeAll().then(async msg => {
                     message.edit(privateCommandsImage)
