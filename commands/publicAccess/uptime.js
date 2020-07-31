54ccbab125a4561a01811372699cdb167e66e7cc
Carlo <carlo@carlos-air.home>
2020-07-31 15:27:20 +0100

    New update

--- commands/publicAccess/uptime.js
+++ commands/publicAccess/uptime.js
@@ -5,6 +5,19 @@ module.exports = {
     name: "uptime",
     category: "publicAccess",
     run: async (bot, message, args) => {
+        function emoji(id) {
+            return bot.emojis.cache.get(id).toString();
+        }
+
+        const ErrorEmoji = "738675252246085691"
+        const ErrorColor = "0xFF5858"
+
+        const SuccessEmoji = "738675745693630474"
+        const SuccessColor = "0x49CE4A"
+
+        const InformationEmoji = "738675785098854480"
+        const InformationColor = "0x009DF9"
+        
         var time = bot.uptime
 
         function dhm(ms) {
