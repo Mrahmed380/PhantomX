32d9785e99c48bc0d341f731f8d2d8f5ecddf800
Carlo <carlo@carlos-air.home>
2020-07-31 14:37:41 +0100

    New update

--- commands/publicAccess/random.js
+++ commands/publicAccess/random.js
@@ -16,22 +16,32 @@ module.exports = {
 
         const InformationEmoji = "738675785098854480"
         const InformationColor = "0x009DF9"
-        
+
         const Number = args[0]
         const Random = Math.floor(Math.random() * (Number))
 
         const Embed1 = new Discord.MessageEmbed()
-            .setTitle(`${emoji(`${WarnEmoji}`)}` + " **COMMAND INFO**")
+            .setTitle(`${emoji(`${InformationEmoji}`)}` + " **COMMAND INFO**")
             .addField("COMMAND", "```Random```", true)
             .addField("PERMISSIONS", "```None```", true)
             .addField("USAGE", "```>random <number>```", true)
             .addField("DESCRIPTION", "```Picks a number from 0 to the number you have chosen.```", true)
-            .setColor(WarnColor)
-        
+            .setColor(InformationColor)
+
         if (!args[0]) {
             message.channel.send(Embed1)
         }
 
         message.channel.send(Random)
+            .catch(err => {
+                if (err) {
+                    const ConsoleEmbed = new Discord.MessageEmbed()
+                        .setTitle(`${emoji(`${ErrorEmoji}`)}` + " **TERMINAL ERROR**")
+                        .setDescription("```" + `${err}` + "```")
+                        .setColor(ErrorColor)
+
+                    return message.channel.send(ConsoleEmbed)
+                }
+            });
     }
 }
