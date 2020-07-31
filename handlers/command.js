63b5f9d3cf08bff289550d684646d758cf898058
Carlo <carlo@carlos-air.home>
2020-07-31 12:05:12 +0100

    New update

--- handlers/command.js
+++ handlers/command.js
@@ -5,7 +5,7 @@ let table = new ascii("Output");
 table.setHeading("File", "Status");
 module.exports = (bot) => {
     readdirSync("./commands/").filter(x => x.includes(".DS_Store") == false).map(dir => {
-        const commands = readdirSync(`./commands/${dir}/`).filter(f => f.includes(".DS_store") == false).map(cmd=>{
+        const commands = readdirSync(`./commands/${dir}/`).filter(file => file.endsWith(".js"));
         for (let file of commands) {
             let pull = require(`../commands/${dir}/${file}`);
             if (pull.name) {
@@ -25,21 +25,6 @@ module.exports = (bot) => {
                 pull.aliases.forEach(alias => bot, aliases.set(alias, pull.name))
             }
         }
-    })
     });
     console.log(table.toString());
-}
-
-
-const { readdirSync } = require("fs");
-
-module.exports = (client) => {
-    readdirSync("./commands/").filter(x => x.includes(".DS_Store") == false).map(dir => {
-       const commands = readdirSync(`./commands/${dir}/`).filter(f => f.includes(".DS_store") == false).map(cmd=>{
-           let pull = require(`../commands/${dir}/${cmd}`)
-           console.log(`Loaded command ${pull.name}`)
-           client.commands.set(pull.name,pull)
-           if (pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => client.aliases.set(alias, pull.name));
-       })
-    })
 }
