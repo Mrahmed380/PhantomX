3617d788691e96f7734813045fe1548ea7cadc85
Carlo <carlo@carlos-air.home>
2020-07-31 15:53:00 +0100

    New update

--- commands/privateAccess/ban.js
+++ commands/privateAccess/ban.js
@@ -34,7 +34,7 @@ module.exports = {
 
         const Embed3 = new Discord.MessageEmbed()
             .setTitle(`${emoji(`${ErrorEmoji}`)}` + " **ERROR**")
-            .setDescription("```" + `Missing permission: Administrator` + "```")
+            .setDescription("```" + `Missing permissions.` + "```")
             .setColor(ErrorColor)
 
         const Embed4 = new Discord.MessageEmbed()
