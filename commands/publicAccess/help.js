5a006d1309d5612425bf924e576d3eff3602c254
Carlo <carlo@carlos-air.home>
2020-07-31 13:47:48 +0100

    New update

--- commands/publicAccess/help.js
+++ commands/publicAccess/help.js
@@ -20,7 +20,6 @@ module.exports = {
             
         const Embed = new Discord.MessageEmbed()
             .setTitle("**HELP**")
-            .addField("**DESCRIPTION**", "```I was made by Phantom and acts as his loyal servant.```", true)
             .addField("**COMMANDS**", "```Say '>commands' to view all the commands.```", true)
             .addField("**PREFIX**", "```>```", true)
             .addField("**INVITE**", "`https://discord.com/oauth2/authorize?client_id=696017289044164648&permissions=8&scope=bot`", true)
