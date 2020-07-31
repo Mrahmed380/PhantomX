26884d5c847004c6f5e0969969c1ab4fb56b4964
Carlo <carlo@carlos-air.home>
2020-07-31 14:55:19 +0100

    New update

--- commands/publicAccess/ping.js
+++ commands/publicAccess/ping.js
@@ -5,6 +5,7 @@ module.exports = {
     category: "publicAccess",
     run: async (bot, message, args) => {
         const Embed = new Discord.MessageEmbed()
+            .setThumbnail(message.author.displayAvatarURL({ dynamic: true, format: "png" }))
             .addField(`**PING **`, "```" + `${Math.round(bot.ws.ping)}` + " ms" + "```", true)
             .setColor(0xFAFAFA)
 
