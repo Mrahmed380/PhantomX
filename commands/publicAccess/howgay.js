e05a88e43459db4cbcad6c64298f284497d223cd
Carlo <carlo@carlos-air.home>
2020-07-31 14:28:10 +0100

    New update

--- commands/publicAccess/howgay.js
+++ commands/publicAccess/howgay.js
@@ -4,19 +4,6 @@ module.exports = {
     name: "howgay",
     category: "publicAccess",
     run: async (bot, message, args) => {
-        function emoji(id) {
-            return bot.emojis.cache.get(id).toString();
-        }
-
-        const ErrorEmoji = "738675252246085691"
-        const ErrorColor = "0xFF5858"
-
-        const SuccessEmoji = "738675745693630474"
-        const SuccessColor = "0x49CE4A"
-
-        const InformationEmoji = "738675785098854480"
-        const InformationColor = "0x009DF9"
-
         const Number = 100
         const Random = Math.floor(Math.random() * (Number))
 
