10a66d948c0015dc0c07970cd7cd91b761b83c3e
Carlo <carlo@carlos-air.home>
2020-07-31 12:08:31 +0100

    New update

--- commands/publicAccess/say.js
+++ commands/publicAccess/say.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports = {
-    name: "say",
+    name: "rep",
     category: "publicAccess",
     run: async (bot, message, args) => {
         function emoji(id) {
