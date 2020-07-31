3bdcd62442fcb8f75bc66109050f66b6db758583
Carlo <carlo@carlos-air.home>
2020-07-31 15:51:50 +0100

    New update

--- commands/privateAccess/announce.js
+++ commands/privateAccess/announce.js
@@ -53,6 +53,7 @@ module.exports = {
 
         const Embed4 = new Discord.MessageEmbed()
             .setTitle(`**New announcement from ${message.author.username}**`)
+            .setThumbnail(message.author.displayAvatarURL({ dynamic: true, format: "png" }))s
             .setDescription(`${announcement}`)
             .setColor(0xFAFAFA)
 
