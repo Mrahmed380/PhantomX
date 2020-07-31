7470f3e8abfce9d81d7748281cbcb8690a6cd9fe
Carlo <carlo@carlos-air.home>
2020-07-31 15:15:33 +0100

    New update

--- commands/publicAccess/timer.js
+++ commands/publicAccess/timer.js
@@ -34,7 +34,7 @@ module.exports = {
 
         const Embed2 = new Discord.MessageEmbed()
             .setTitle(`${emoji(`${SuccessEmoji}`)}` + " **TIMER**")
-            .setAuthor(`${message.author.displayAvatarURL()}`, `\u200b`)
+            .setAuthor(`\u200b`, `${message.author.displayAvatarURL()}`)
             .setDescription(`Timer set: ` + ms(ms(Timer), { long: true }))
             .setColor(SuccessColor)
 
