d488044381c4bd42aea0147354db32eeb92e175d
Carlo <carlo@carlos-air.home>
2020-07-31 18:29:48 +0100

    New update

--- commands/privateAccess/report.js
+++ commands/privateAccess/report.js
@@ -35,7 +35,7 @@ module.exports = {
 
         const Embed3 = new Discord.MessageEmbed()
             .setTitle(`${emoji(`${ErrorEmoji}`)}` + " **ERROR**")
-            .setDescription("```" + `Missing permission.` + "```")
+            .setDescription("```" + `Missing permissions.` + "```")
             .setColor(ErrorColor)
 
         const Embed4 = new Discord.MessageEmbed()
