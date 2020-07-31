c79925ffa95833e90bcbbc032c4b29b56dab2bbc
Carlo <carlo@carlos-air.home>
2020-07-31 15:29:26 +0100

    New update

--- commands/publicAccess/serverinfo.js
+++ commands/publicAccess/serverinfo.js
@@ -20,7 +20,7 @@ module.exports = {
         const Embed = new Discord.MessageEmbed()
             .setDescription("**SERVER INFORMATION**")
             .setColor(0xFAFAFA)
-            .setThumbnail(message.author.displayAvatarURL({ dynamic: true, format: "png" }))
+            .setThumbnail(bot.imageURL)
             .addField("**SERVER NAME:**", "```" + message.guild.name + "```", true)
             .addField("**SERVER OWNER:**", "```" + message.guild.owner.user.tag + "```", true)
             .addField("**SERVER REGION:**", "```" + message.guild.region + "```", true)
