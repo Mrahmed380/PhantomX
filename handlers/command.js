537281d014be5de4f99481587a4af7369f93b67b
Carlo <carlo@carlos-air.home>
2020-07-31 11:59:04 +0100

    New update

--- handlers/command.js
+++ handlers/command.js
@@ -1,11 +1,35 @@
 const { readdirSync } = require("fs");
-module.exports = (client) => {
-    readdirSync("./commands/").map(dir => {
-       const commands = readdirSync(`./commands/${dir}/`).map(cmd=>{
-           let pull = require(`../commands/${dir}/${cmd}`)
-           console.log(`Loaded command ${pull.name}`)
-           client.commands.set(pull.name,pull)
-           if (pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => client.aliases.set(alias, pull.name));
-       })
-    })
+const ascii = require("ascii-table");
+let table = new ascii("Output");
+
+table.setHeading("File", "Status");
+module.exports = (bot) => {
+    readdirSync("./commands/").forEach(dir => {
+        const commands = readdirSync(`./commands/${dir}/`).map(cmd=>{
+            let pull = require(`../commands/${dir}/${cmd}`)
+            console.log(`Loaded command ${pull.name}`)
+            client.commands.set(pull.name,pull)
+            if (pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => client.aliases.set(alias, pull.name));
+        })
+        for (let file of commands) {
+            let pull = require(`../commands/${dir}/${file}`);
+            if (pull.name) {
+                bot.commands.set(pull.name, pull);
+                table.addRow(file, "✔️")
+            } else {
+                table.addRow(file, "✖️ -> ERROR")
+                setTimeout(() => {
+                    console.log("✖️ ERROR DETECED")
+                }, 100);
+                setTimeout(() => {
+                    console.log("------------------------------------------")
+                }, 300);
+                continue;
+
+            } if (pull.aliases && Array.isArray(pull.aliases)) {
+                pull.aliases.forEach(alias => bot, aliases.set(alias, pull.name))
+            }
+        }
+    });
+    console.log(table.toString());
 }
