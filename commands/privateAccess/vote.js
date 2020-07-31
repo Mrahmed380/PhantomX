2fdc6ae84fbdb86c2036eeecf16cf5fe64d899ed
Carlo <carlo@carlos-air.home>
2020-07-31 18:44:35 +0100

    New update

--- commands/privateAccess/vote.js
+++ commands/privateAccess/vote.js
@@ -61,17 +61,17 @@ module.exports = {
             .catch(err => {
                 if (err) {
                     const ConsoleEmbed = new Discord.MessageEmbed()
-                        .setTitle(`${emoji(`${AdminNotificationEmoji}`)}` + " **TERMINAL ERROR**")
+                        .setTitle(`${emoji(`${ErrorEmoji}`)}` + " **TERMINAL ERROR**")
                         .setDescription("```" + `${err}` + "```")
-                        .setColor(AdminNotificationColor)
+                        .setColor(ErrorColor)
 
                     return message.channel.send(ConsoleEmbed)
                         .catch(err => {
                             if (err) {
                                 const ConsoleEmbed = new Discord.MessageEmbed()
-                                    .setTitle(`${emoji(`${AdminNotificationEmoji}`)}` + " **TERMINAL ERROR**")
+                                    .setTitle(`${emoji(`${ErrorEmoji}`)}` + " **TERMINAL ERROR**")
                                     .setDescription("```" + `${err}` + "```")
-                                    .setColor(AdminNotificationColor)
+                                    .setColor(ErrorColor)
 
                                 return message.channel.send(ConsoleEmbed)
                             }
