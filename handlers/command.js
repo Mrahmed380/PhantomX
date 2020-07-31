8e712827be3b8237cc2cb736ecc49af7c1a5b481
Carlo <carlo@carlos-air.home>
2020-07-31 12:03:32 +0100

    New update

--- handlers/command.js
+++ handlers/command.js
@@ -5,7 +5,7 @@ let table = new ascii("Output");
 table.setHeading("File", "Status");
 module.exports = (bot) => {
     readdirSync("./commands/").forEach(dir => {
-        const commands = readdirSync(`./commands/${dir}/`).filter(f => f.includes(".DS_store") == false).map(cmd=>{
+        const commands = readdirSync(`./commands/${dir}/`).filter(file => file.endsWith(".js"));
         for (let file of commands) {
             let pull = require(`../commands/${dir}/${file}`);
             if (pull.name) {
@@ -25,15 +25,13 @@ module.exports = (bot) => {
                 pull.aliases.forEach(alias => bot, aliases.set(alias, pull.name))
             }
         }
-        }
     });
     console.log(table.toString());
 }
 
 
 const { readdirSync } = require("fs");
-const ascii = require("ascii-table");
-let table = new ascii("Output");
+
 module.exports = (client) => {
     readdirSync("./commands/").filter(x => x.includes(".DS_Store") == false).map(dir => {
        const commands = readdirSync(`./commands/${dir}/`).filter(f => f.includes(".DS_store") == false).map(cmd=>{
