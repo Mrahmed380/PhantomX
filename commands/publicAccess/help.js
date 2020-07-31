9bd105ec747c0b81d9e20e9878c0e9310f950d8f
Carlo <carlo@carlos-air.home>
2020-07-31 13:48:38 +0100

    New update

--- commands/publicAccess/help.js
+++ commands/publicAccess/help.js
@@ -21,8 +21,8 @@ module.exports = {
         const Embed = new Discord.MessageEmbed()
             .setTitle("**HELP**")
             .addField("**INVITE**", "`https://discord.com/oauth2/authorize?client_id=696017289044164648&permissions=8&scope=bot`", true)
-            .addField("**COMMANDS**", "```Say '>commands' to view all the commands.```", true)
-            .addField("**PREFIX**", "```>```", true)
+            .addField("**COMMANDS**", "`Say '>commands' to view all the commands.`", true)
+            .addField("**PREFIX**", "`>`", true)
             .setColor(0xFAFAFA);
 
         message.author.send(Embed)
