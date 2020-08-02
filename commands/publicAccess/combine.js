293fc7e54a8bbfdd393f1351962090f9ebe53f9a
Carlo <carlo@carlos-air.home>
2020-08-02 10:53:12 +0100

    New update

--- commands/publicAccess/combine.js
+++ commands/publicAccess/combine.js
@@ -44,7 +44,7 @@ module.exports = {
         const FirstUsername = FirstUser.user.username
         const FirstUsernameSplit = FirstUsername.split(FirstUsername.length + FirstUsername.length)
 
-        const avatarList = message.mentions.users.map(user => {
+        const SecondUser = message.mentions.users.map(user => {
             return `${user.username}'s avatar: <${user.displayAvatarURL({ format: "png", dynamic: true })}>`;
         });
         const SecondUser = message.mentions.members.first(-1) || message.guild.members.cache.get(args[1])
