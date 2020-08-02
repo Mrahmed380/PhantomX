0225f54e8f55820fff1b1e9691499d2b1f5fd4ab
Carlo <carlo@carlos-air.home>
2020-08-02 11:01:03 +0100

    New update

--- commands/publicAccess/combine.js
+++ commands/publicAccess/combine.js
@@ -44,7 +44,7 @@ module.exports = {
         const SecondUser = message.mentions.members.first(-1) || message.guild.members.cache.get(args[1])
 
         const Users = message.mentions.users.map(user => {
-            return user.username + "+" +  user.username
+            return FirstUser.username + "+" +  SecondUser.username
         });
         if (!FirstUser) {
             return message.channelsend(Embed2)
