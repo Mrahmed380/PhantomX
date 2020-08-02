73c80f26bc0f0048a142358261da5a94b8da4329
Carlo <carlo@carlos-air.home>
2020-08-02 09:42:52 +0100

    New update

--- commands/publicAccess/combine.js
+++ commands/publicAccess/combine.js
@@ -45,7 +45,7 @@ module.exports = {
 
         const Embed = new Discord.MessageEmbed()
             .setTitle("NAME COMBINING MACHINE")
-            .setDescription(`${FirstUser} + ${SecondUser} = ${FirstUser.user.username}${SecondUser.user.tag}`)
+            .setDescription(`${FirstUser} + ${SecondUser} = ${FirstUser.user.username}${SecondUser}`)
             .setColor(0xFAFAFA)
 
         message.channel.send(Embed)
