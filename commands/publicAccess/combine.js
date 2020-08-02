48096b673ecdc13d93692e6bebc88f66ae4dcd6e
Carlo <carlo@carlos-air.home>
2020-08-02 10:44:25 +0100

    New update

--- commands/publicAccess/combine.js
+++ commands/publicAccess/combine.js
@@ -43,7 +43,7 @@ module.exports = {
         const FirstUser = message.mentions.members.first() || message.guild.members.cache.get(args[0])
         const FirstUsername = FirstUser.user.username
 
-        const SecondUser = message.mentions.members.first() || message.guild.members.cache.get(args[1])
+        const SecondUser = message.mentions.members.first(args[1]) || message.guild.members.cache.get(args[1])
         const SecondUsername = SecondUser.user.username
 
         const FirstUsernameSplit = FirstUsername.slice(FirstUsername.length / 2)
