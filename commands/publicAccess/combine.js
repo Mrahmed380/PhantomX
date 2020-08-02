526f8d9711ede48ab2826358d00ff6e84e406b4f
Carlo <carlo@carlos-air.home>
2020-08-02 10:22:17 +0100

    New update

--- commands/publicAccess/combine.js
+++ commands/publicAccess/combine.js
@@ -42,6 +42,7 @@ module.exports = {
 
         const FirstUser = message.mentions.members.first() || message.guild.members.get(args[0]);
         const SecondUser = message.mentions.members.first(-1) || message.guild.members.get(args[1]);
+        const FirstUsername = FirstUser
 
         const Embed = new Discord.MessageEmbed()
             .setTitle("NAME COMBINING MACHINE")
